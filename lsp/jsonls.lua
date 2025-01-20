0ffe03bd4dab9ff574df8390e38d12c6c7569c6b
Jonathan Raines <jonathan.s.raines@gmail.com>
2025-01-20 08:27:14 -0600

    Enable formatting on jsonls

--- lsp/jsonls.lua
+++ lsp/jsonls.lua
@@ -2,5 +2,8 @@ return {
   cmd = { 'vscode-json-language-server', '--stdio' },
   filetypes = { 'json', 'jsonc' },
   root_markers = { '.git' },
-  single_file_support = true
+  single_file_support = true,
+  init_options = {
+    provideFormatter = true,
+  }
 }
