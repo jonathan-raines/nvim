1db3543c4b9a0e1efe1812b6fa96dec0e6a964e0
Jonathan Raines <jonathan.s.raines@gmail.com>
2025-02-04 09:26:47 -0600

    Replace netrw with mini.files

--- after/plugin/mini-files.lua
+++ after/plugin/mini-files.lua
@@ -1,5 +1,9 @@
 MiniDeps.now(function()
-  require 'mini.files'.setup {}
+  require 'mini.files'.setup {
+    options = {
+      use_as_default_explorer = true
+    }
+  }
 
   vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open()<CR>', { desc = 'File Explorer' })
   vim.keymap.set('n', '-', '<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0), true)<CR>',
