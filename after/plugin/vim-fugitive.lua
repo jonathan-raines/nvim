2e22a77ca678a196b91b9315f78d3d9dde9ecf3b
Jonathan Raines <jonathan.s.raines@gmail.com>
2025-02-06 15:00:21 -0600

    Replace fugitive with neogit

--- after/plugin/vim-fugitive.lua
+++ after/plugin/vim-fugitive.lua
@@ -1,5 +1,5 @@
-MiniDeps.add { source = 'tpope/vim-fugitive' }
+MiniDeps.add { source = "tpope/vim-fugitive" }
 
 MiniDeps.later(function()
-  vim.keymap.set('n', '<leader>gs', [[:tab G<CR>]], { desc = '[Fugitive] Status' })
+  vim.keymap.set('n', '<leader>gs', [[:tab Git<CR>]], { desc = '[Git] Status' })
 end)
