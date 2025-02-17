024eefbd80d8b2e2543b7865b8758f8c0661f9db
Jonathan Raines <jonathan.s.raines@gmail.com>
2025-02-17 09:11:30 -0600

    Plugin updates

--- after/plugin/blink.cmp.lua
+++ after/plugin/blink.cmp.lua
@@ -1,20 +1,6 @@
-local function build_blink(params)
-  vim.notify('Building blink.cmp', vim.log.levels.INFO)
-  local obj = vim.system({ 'cargo', 'build', '--release' }, { cwd = params.path }):wait()
-  if obj.code == 0 then
-    vim.notify('Building blink.cmp done', vim.log.levels.INFO)
-  else
-    vim.notify('Building blink.cmp failed', vim.log.levels.ERROR)
-  end
-end
-
 MiniDeps.add({
   source = 'Saghen/blink.cmp',
   checkout = 'v0.11.0',
-  -- hooks = {
-  --   post_install = build_blink,
-  --   post_checkout = build_blink,
-  -- },
 })
 
 MiniDeps.later(function()
