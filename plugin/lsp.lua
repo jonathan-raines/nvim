729de478faab6d25b73949fd3fa637e7a5b4d4fc
Jonathan Raines <jonathan.s.raines@gmail.com>
2025-04-23 10:21:20 -0500

    Update lsp config

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -4,6 +4,7 @@
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id) or {}
+    local methods = vim.lsp.protocol.Methods
 
     -- Keymaps
     local function keymap(lhs, rhs, desc, mode)
@@ -16,18 +17,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
     keymap('<leader>ls', function() vim.lsp.stop_client(vim.lsp.get_clients()) end, 'Stop LSP Servers')
 
     -- Symbols
-    if client:supports_method("textDocument/documentSymbol") then
       keymap("<leader>fs", function() Snacks.picker.lsp_symbols() end, "LSP Symbols")
+    if client:supports_method(methods.textDocument_documentSymbol) then
     end
 
     -- Inlay Hints
-    if client:supports_method("textDocument/inlayHint") then
+    if client:supports_method(methods.textDocument_inlayHint) then
       keymap('<leader>lh', function()
         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ 0 }))
       end, 'Inlay Hints')
     end
 
-    if client:supports_method('textDocument/formatting') or client.name == 'solargraph' then
+    if client:supports_method(methods.textDocument_formatting) or client.name == 'solargraph' then
       if client.name ~= 'tsserver' then
         -- Format the current buffer on save
         vim.api.nvim_create_autocmd('BufWritePre', {
@@ -50,21 +51,22 @@ vim.api.nvim_create_autocmd('LspDetach', {
   callback = function(args)
     -- Get the detaching client
     local client = vim.lsp.get_client_by_id(args.data.client_id) or {}
+    local methods = vim.lsp.protocol.Methods
 
     -- Remove the autocommand to format the buffer on save, if it exists
-    if client:supports_method('textDocument/formatting') then
+    if client:supports_method(methods.textDocument_formatting) then
       vim.api.nvim_clear_autocmds({
         event = 'BufWritePre',
         buffer = args.buf,
       })
     end
 
-    if client:supports_method("textDocument/documentSymbol") then
+    if client:supports_method(methods.textDocument_documentSymbol) then
       vim.keymap.del('n', '<leader>fs', { buffer = args.buf })
     end
 
     -- Inlay Hints
-    if client:supports_method("textDocument/inlayHint") then
+    if client:supports_method(methods.textDocument_inlayHint) then
       vim.keymap.del('n', '<leader>lh', { buffer = args.buf })
     end
   end
