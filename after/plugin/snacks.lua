821e3b1a2cb4bdca7d19b633d5743b70aedb5b69
Jonathan Raines <jonathan.s.raines@gmail.com>
2025-05-16 13:33:05 -0500

    Update snacks keymaps

--- after/plugin/snacks.lua
+++ after/plugin/snacks.lua
@@ -22,40 +22,43 @@ MiniDeps.later(function()
     }
   }
 
-  --  ╭─────────────────────────────────────────────────────────╮
-  --  │                     Explorer                            │
-  --  ╰─────────────────────────────────────────────────────────╯
-  vim.keymap.set('n', "<leader>fe", function() Snacks.explorer() end, { desc = "File Explorer" })
-  --  ╭─────────────────────────────────────────────────────────╮
-  --  │                     Git                                 │
-  --  ╰─────────────────────────────────────────────────────────╯
-  vim.keymap.set('n', '<leader>gg', function() Snacks.lazygit() end, { desc = 'Lazygit' })
-  vim.keymap.set('n', '<leader>gf', function() Snacks.lazygit.log_file() end, { desc = 'Lazygit Current File History' })
-  vim.keymap.set('n', '<leader>gl', function() Snacks.lazygit.log() end, { desc = 'Lazygit Log (cwd)' })
-  vim.keymap.set('n', "<leader>fh", function() Snacks.picker.git_status() end, { desc = "Git Status" })
-  --  ╭─────────────────────────────────────────────────────────╮
-  --  │                     Picker                              │
-  --  ╰─────────────────────────────────────────────────────────╯
-  vim.keymap.set('n', '<leader><space>', function() Snacks.picker() end, { desc = 'Pickers' })
-  vim.keymap.set('n', "<leader>:", function() Snacks.picker.command_history() end, { desc = "Command History" })
-  vim.keymap.set('n', "<leader>/", function() Snacks.picker.grep() end, { desc = "Grep" })
-  vim.keymap.set('n', "<leader>f/", function() Snacks.picker.lines() end, { desc = "Buffer Lines" })
-  vim.keymap.set('n', "<leader>bl", function() Snacks.picker.buffers() end, { desc = "Buffers" })
-  vim.keymap.set('n', "<leader>fa", function()
-    Snacks.picker.smart { filter = { cwd = vim.fn.getcwd() }, multi = { "buffers", "recent", "files", "git_status" } }
-  end, { desc = "Smart" })
-  vim.keymap.set('n', "<leader>fb", function() Snacks.picker.grep_buffers() end, { desc = "Grep Open Buffers" })
-  vim.keymap.set('n', "<leader>fc", function() Snacks.picker.commands() end, { desc = "Commands" })
-  vim.keymap.set('n', "<leader>fC", function() Snacks.picker.colorschemes() end, { desc = "Colorschemes" })
-  vim.keymap.set('n', '<leader>ff', function() Snacks.picker.files() end, { desc = 'Files' })
-  vim.keymap.set('n', "<leader>fo", function()
-    Snacks.picker.recent { filter = { cwd = vim.fn.getcwd() } }
-  end, { desc = "Recent" })
-  vim.keymap.set('n', "<leader>fr", function() Snacks.picker.resume() end, { desc = "Resume" })
-  vim.keymap.set({ "n", "x" }, "<leader>fw", function() Snacks.picker.grep_word() end,
-    { desc = "Visual selection or word" })
-  --  ╭─────────────────────────────────────────────────────────╮
-  --  │                     Terminal                            │
-  --  ╰─────────────────────────────────────────────────────────╯
-  vim.keymap.set({ 'n', 't' }, '<c-\\>', function() Snacks.terminal() end, { desc = 'Toggle Terminal' })
+  local keymaps = {
+    --  ╭─────────────────────────────────────────────────────────╮
+    --  │                     Explorer                            │
+    --  ╰─────────────────────────────────────────────────────────╯
+    { 'n',          "<leader>fe",      function() Snacks.explorer() end,                                                                                                  { desc = "File Explorer" } },
+    --  ╭─────────────────────────────────────────────────────────╮
+    --  │                     Git                                 │
+    --  ╰─────────────────────────────────────────────────────────╯
+    { 'n',          '<leader>gg',      function() Snacks.lazygit() end,                                                                                                   { desc = 'Lazygit' } },
+    { 'n',          '<leader>gf',      function() Snacks.lazygit.log_file() end,                                                                                          { desc = 'Lazygit Current File History' } },
+    { 'n',          '<leader>gl',      function() Snacks.lazygit.log() end,                                                                                               { desc = 'Lazygit Log (cwd)' } },
+    { 'n',          "<leader>fh",      function() Snacks.picker.git_status() end,                                                                                         { desc = "Git Status" } },
+    --  ╭─────────────────────────────────────────────────────────╮
+    --  │                     Picker                              │
+    --  ╰─────────────────────────────────────────────────────────╯
+    { 'n',          '<leader><space>', function() Snacks.picker() end,                                                                                                    { desc = 'Pickers' } },
+    { 'n',          "<leader>:",       function() Snacks.picker.command_history() end,                                                                                    { desc = "Command History" } },
+    { 'n',          "<leader>/",       function() Snacks.picker.grep() end,                                                                                               { desc = "Grep" } },
+    { 'n',          "<leader>f/",      function() Snacks.picker.lines() end,                                                                                              { desc = "Buffer Lines" } },
+    { 'n',          "<leader>bl",      function() Snacks.picker.buffers() end,                                                                                            { desc = "Buffers" } },
+    { 'n',          "<leader>fa",      function() Snacks.picker.smart { filter = { cwd = vim.fn.getcwd() }, multi = { "buffers", "recent", "files", "git_status" } } end, { desc = "Smart" } },
+    { 'n',          "<leader>fb",      function() Snacks.picker.grep_buffers() end,                                                                                       { desc = "Grep Open Buffers" } },
+    { 'n',          "<leader>fc",      function() Snacks.picker.commands() end,                                                                                           { desc = "Commands" } },
+    { 'n',          "<leader>fC",      function() Snacks.picker.colorschemes() end,                                                                                       { desc = "Colorschemes" } },
+    { 'n',          '<leader>ff',      function() Snacks.picker.files() end,                                                                                              { desc = 'Files' } },
+    { 'n',          "<leader>fo",      function() Snacks.picker.recent { filter = { cwd = vim.fn.getcwd() } } end,                                                        { desc = "Recent" } },
+    { 'n',          "<leader>fr",      function() Snacks.picker.resume() end,                                                                                             { desc = "Resume" } },
+    { { "n", "x" }, "<leader>fw",      function() Snacks.picker.grep_word() end,                                                                                          { desc = "Visual selection or word" } },
+    --  ╭─────────────────────────────────────────────────────────╮
+    --  │                     Terminal                            │
+    --  ╰─────────────────────────────────────────────────────────╯
+    { { 'n', 't' }, '<c-\\>',          function() Snacks.terminal() end,                                                                                                  { desc = 'Toggle Terminal' } },
+  }
+
+  for _, val in pairs(keymaps) do
+    vim.keymap.set(val[1], val[2], val[3], vim.tbl_extend('force', {}, val[4]))
+  end
+
+  vim.cmd([[au FileType snacks_picker_input lua vim.b.minicompletion_disable = true]])
 end)
