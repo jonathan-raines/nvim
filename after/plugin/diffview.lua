bb1e3d58964a5ee75eead872a2ceaa213032651b
Jonathan Raines <jonathan.s.raines@gmail.com>
2025-08-25 09:27:31 -0500

    Remove diffview

--- after/plugin/diffview.lua
+++
@@ -1,6 +0,0 @@
-vim.pack.add({ 'https://github.com/sindrets/diffview.nvim' }, { load = true })
-
-vim.keymap.set('n', '<leader>do', vim.cmd.DiffviewOpen, { desc = '[Diffview] Open' })
-vim.keymap.set('n', '<leader>dc', vim.cmd.DiffviewClose, { desc = '[Diffview] Close' })
-vim.keymap.set('n', '<leader>df', function() vim.cmd.DiffviewFileHistory '%' end, { desc = '[Diffview] File History' })
-vim.keymap.set('n', '<leader>dh', vim.cmd.DiffviewFileHistory, { desc = '[Diffview] History' })
