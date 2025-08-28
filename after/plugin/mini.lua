b61b99a428e48abb014fc04ad7c7ac8e8cd5d546
Jonathan Raines <jonathan.s.raines@gmail.com>
2025-08-28 09:13:46 -0500

    Update mini url

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -1,4 +1,4 @@
-vim.pack.add { 'https://github.com/echasnovski/mini.nvim' }
+vim.pack.add { 'https://github.com/nvim-mini/mini.nvim' }
 
 --  ╭─────────────────────────────────────────────────────────╮
 --  │                     Mini.Completion                     │
