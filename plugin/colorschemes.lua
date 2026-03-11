c96fde7186c8a76e874c0b3e517eab0738b09b72
Jonathan Raines <jonathan.s.raines@gmail.com>
2026-03-11 15:21:33 -0500

    Add monokai pro colorscheme

--- plugin/colorschemes.lua
+++ plugin/colorschemes.lua
@@ -1,3 +1,7 @@
 vim.pack.add { 'https://github.com/folke/tokyonight.nvim' }
+vim.pack.add { 'https://github.com/EdenEast/nightfox.nvim' }
+vim.pack.add { 'https://github.com/loctvl842/monokai-pro.nvim' }
 
-vim.cmd.colorscheme 'tokyonight-night'
+require("monokai-pro").setup { transparent_background = true }
+
+vim.cmd.colorscheme 'monokai-pro-spectrum'
