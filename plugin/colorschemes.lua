259946a94b7005ba164db011f1e8b6656caeaf94
Jonathan Raines <jonathan.s.raines@gmail.com>
2026-04-16 15:15:11 -0500

    Remove tokyonight colorscheme

--- plugin/colorschemes.lua
+++ plugin/colorschemes.lua
@@ -1,4 +1,3 @@
-vim.pack.add { 'https://github.com/folke/tokyonight.nvim' }
 vim.pack.add { 'https://github.com/loctvl842/monokai-pro.nvim' }
 
 require("monokai-pro").setup { transparent_background = true }
